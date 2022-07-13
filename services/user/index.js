e4e6f0bbb6609e116c8f631db72abcb11bc185d1
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-13 15:19:50 +0200

    user test endpoint implemented

--- services/user/index.js
+++ services/user/index.js
@@ -2,6 +2,12 @@ const router = require('express').Router();
 const { authorizer } = require('../../middlewares');
 
 module.exports = db => {
+  router.get('/test', (req, res, next) => {
+    res.status(200).json({
+      success: true,
+      message: 'ok',
+    });
+  });
   router.get('/', authorizer, require('./get-user')(db));
   router.patch('/', authorizer, require('./update-user')(db));
 
