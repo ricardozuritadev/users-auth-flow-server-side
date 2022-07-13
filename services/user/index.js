b1adf535ea7f225962f8961f3748335743f17306
Ricardo Zurita Baldeon <rzb@cyc.es>
2022-07-13 15:22:46 +0200

    user test endpint deleted

--- services/user/index.js
+++ services/user/index.js
@@ -2,12 +2,6 @@ const router = require('express').Router();
 const { authorizer } = require('../../middlewares');
 
 module.exports = db => {
-  router.get('/test', (req, res, next) => {
-    res.status(200).json({
-      success: true,
-      message: 'ok',
-    });
-  });
   router.get('/', authorizer, require('./get-user')(db));
   router.patch('/', authorizer, require('./update-user')(db));
 
